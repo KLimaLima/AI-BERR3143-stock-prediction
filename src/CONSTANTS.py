776c2598cdacea09c311d76881c96e123c9c103e
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-10-28 22:27:13 +0800

    video tutorial in slides

---
+++ src/CONSTANTS.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+START = '2015-01-01'
+TODAY = date.today().strftime('%Y-%m-%d')
+
+WEB_TITLE = 'UTeM GPT Stock Prediction Web Application'
+
+STOCKS = ('AAPL', 'GOOGL', 'MSFT', 'GME')
+
+SELECT_BOX_MSG = 'Select dataset for prediction'
+SLIDER_MSG = 'Year of prediction:'
+LOADING = 'Loading data...'
+
+RAW_DATA = 'Raw data'
