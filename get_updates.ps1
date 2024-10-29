d8fe78d7c183ef95290eb6715fcb093cd95b977c
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-10-30 00:24:11 +0800

    Create get_updates.ps1

---
+++ get_updates.ps1
@@ -0,0 +1,31 @@
+Write-Host "=========================================================="
+Write-Host ">> Getting updates from GitHub"
+git pull
+
+Write-Host "
+   _________________________________
+  |                                 |
+  | Getting updates from GitHub!!!  |
+  |_________________________________|
+            /
+(^.^)/~~~
+"
+
+Write-Host "=========================================================="
+Start-Sleep -Seconds 1.5
+
+Write-Host ">> Finished updating"
+
+Write-Host "
+   _________________________________
+  |                                 |
+  | Here you go. Now suffer.        |
+  |_________________________________|
+            /
+{\__/}
+(o_o)
+( >(That shit code you made)
+"
+
+Write-Host "=========================================================="
+Start-Sleep -Seconds 1.5
