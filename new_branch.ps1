bbd98a6e35f044a9bb250fa0ffa3a602d50ea3f9
KLimaLima <138441635+KLimaLima@users.noreply.github.com>
2024-10-29 22:11:49 +0800

    now can create and move to branch
    - creates new branch
    - moves to that new branch
    - uploads it to github
    - and prints all locally available branches in the machine

---
+++ new_branch.ps1
@@ -0,0 +1,56 @@
+param ([Parameter(Mandatory)]$branch_name)
+
+Write-Host "=========================================================="
+Write-Host ">> Creating a new branch named $branch_name"
+git branch $branch_name
+
+Write-Host "
+{\__/}
+(o_o)
+( >You want branch?
+"
+Write-Host "=========================================================="
+Start-Sleep -Seconds 1.5
+
+Write-Host ">> Moving to $branch_name"
+git checkout $branch_name
+
+Write-Host "
+   _____________________________
+  |                             |
+  | Moving to the new branch!!! |
+  |_____________________________|
+        /
+(^o^)/
+"
+Write-Host "=========================================================="
+Start-Sleep -Seconds 1.5
+
+Write-Host ">> Uploading $branch_name to github"
+git push -u origin $branch_name
+
+Write-Host "
+   _________________________
+  |                         |
+  | Uploading to GitHub!!!  |
+  |_________________________|
+            /
+(^.^)/~~~
+"
+Write-Host "=========================================================="
+Start-Sleep -Seconds 1.5
+
+Write-Host "
+   _____________________________________________________________________
+  |                                                                     |
+  | okay finished! Here are the current branches in your local machine. |
+  | You are at * and green branch, the red branch are in GitHub and     |
+  | others are locally in your machine aka laptop.                      |
+  |_____________________________________________________________________|
+        /
+(~^-^)~
+"
+
+git branch
+Write-Host "=========================================================="
+Start-Sleep -Seconds 1.5
